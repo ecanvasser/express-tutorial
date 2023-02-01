28c93e76205bffecd2d4d3cc80102b71f63ea259
ecanvasser <eddiecanvassere9@gmail.com>
2023-02-01 00:28:15 -0800

    Initialize express.static middleware for static files

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.static("./public/html"));
+
 app.get("/", (req, res) => {
-  res.send("test");
+  res.sendFile("index.html");
 });
 
 app.listen(port);
