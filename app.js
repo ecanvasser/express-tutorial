466388ead1cb91574dd4aba2a9b8b67d9616151e
ecanvasser <eddiecanvassere9@gmail.com>
2023-02-01 01:01:43 -0800

    Add remaining html files and routes in app.js

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.use(express.static("./public/html"));
-
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile(__dirname + "/public/html/index.html");
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + "/public/html/contact.html");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/public/html/about.html");
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + "/public/html/404.html");
 });
 
 app.listen(port);
